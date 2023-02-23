19d507dacded79a958e4242fd0dbe84bb3fb4870
Eduardo Raiol <marquesraiol@gmail.com>
2023-02-23 19:30:59 -0300

    Criando novos vídeos via POST

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -6,6 +6,25 @@ async function listaVideos() {
     return conexaoconvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch(urlApi, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoconvertida = await conexao.json();
+    return conexaoconvertida;
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
